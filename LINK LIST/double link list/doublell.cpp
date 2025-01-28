6a6171b479dcb1ea4ca7a71f54c2f01c84227223
MOHIT SAHU <141635515+MOHITSAHU5649@users.noreply.github.com>
2025-01-29 01:53:49 +0530

    all

---
+++ LINK LIST/double link list/doublell.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+Node(int val){
+    data = val;
+    next = prev =NULL;
+}
+};
+
+class doublell{
+    public:
+    Node* head = NULL;
+    Node* tail = NULL;
+
+void push_front(int val){
+    Node* newnode =new Node(val);
+
+    if(head == NULL){
+        head = tail = newnode;
+    }else{
+        newnode->next = head;
+        head->prev = newnode;
+        head = newnode;
+    }
+}
+
+void printall(){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }cout<<endl;
+}
+
+
+};
+
+
+int main(){
+    doublell ll;
+    ll.push_front(5);
+    ll.push_front(5);
+    ll.push_front(5);
+
+    ll.printall();
+
+    return 0;
+}
