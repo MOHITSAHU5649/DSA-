0aed4bc229b22b6158992ed5ba89919a587fcb44
MOHIT SAHU <141635515+MOHITSAHU5649@users.noreply.github.com>
2025-05-18 00:31:52 +0530

    Queu

---
+++ QUEUE/queueimpl.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+//1. using linklist 
+class Node{
+     public:
+    int data;
+    Node* next;
+   
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class Queue{
+    Node * head;
+    Node * tail;
+    public:
+    
+    Queue(){
+        head = tail = NULL;
+    }
+
+    void push(int val){
+        Node  * newNode = new Node(val);
+        if(empty()){
+            head = tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+    }
+
+    void pop()
+{if(empty()){
+    cout<<"it ios mepty";
+    return;
+}
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+int front(){
+    if(empty()){
+        cout<<"empty";
+        return -1;
+    }else{
+        return head->data;
+    }
+}
+
+bool empty(){
+    return head ==  NULL;
+}
+};
+
+
+int main(){
+    Queue q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+while(!q.empty()){
+    cout<<q.front();
+    q.pop();
+}
+
+return 0;
+
+}
