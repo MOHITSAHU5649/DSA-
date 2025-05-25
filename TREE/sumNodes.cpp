1210d33b2e3482a13c2c53b3ffb924104d4b5497
MOHIT SAHU <141635515+MOHITSAHU5649@users.noreply.github.com>
2025-05-25 12:03:54 +0530

    sum of node

--- TREE/sumNodes.cpp
+++ TREE/sumNodes.cpp
@@ -12,31 +12,31 @@ class Node{
         left = right = NULL;
     }
 };
-static int index  = -1;
+
+static int index =-1;
 Node* buildtree(vector<int> preorder){
     index++;
-
     if(preorder[index] == -1) return NULL;
 
     Node* root = new Node(preorder[index]);
-    root->left = buildtree(preorder);
-    root->right = buildtree(preorder);
-        return root;
+    root->left = buildtree(preorder); 
+    root->right = buildtree(preorder); 
+return root;
+
 }
 
-int count(Node* root){
+int sum(Node* root){
     if(root == NULL) return 0;
 
-    int leftcoutn = count(root->left); //count the left nodes
-    int rightcoutn = count(root->right); // coutn the right nodees
-    return leftcoutn + rightcoutn + 1;
+    int leftsum = sum(root->left);
+    int rightsum = sum(root->right);
+    return leftsum + rightsum + root->data;
 }
 
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
     Node* root = buildtree(preorder);
 
-    cout<<count(root);
-    
+    cout<<sum(root);
     return 0;
 }
